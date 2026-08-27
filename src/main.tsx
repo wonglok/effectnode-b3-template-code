35696c75872a356f0f9f039cee9f4c3e45a9b7cf
Lok Lok <yellowhappy831@gmail.com>
2026-08-27 22:59:07 +0800

    save

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import { AppRouter } from "./AppRouter.tsx";
 
