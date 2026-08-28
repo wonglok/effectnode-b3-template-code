97d6232b15ab29546d59e782c82090d77f8b3f0d
Lok Lok <yellowhappy831@gmail.com>
2026-08-29 06:34:54 +0800

    save

--- src/b3/b3-runtime/src/components/custom/LoadReflection.tsx
+++ src/b3/b3-runtime/src/components/custom/LoadReflection.tsx
@@ -1,7 +1,7 @@
 import { useThree } from "@react-three/fiber";
 import { useEffect } from "react";
 import { Mesh,  RepeatWrapping, SRGBColorSpace, TextureLoader } from "three";
-import { Fn, vec2, vec4, texture, uv, textureBicubic, rangeFogFactor, reflector, time } from 'three/tsl';
+import { Fn, vec2, vec4, texture, uv, textureBicubic, rangeFogFactor, reflector, time, float } from 'three/tsl';
 import { MeshPhysicalNodeMaterial, MeshStandardNodeMaterial } from "three/webgpu";
 
 export function LoadReflection ({  objects = [] }) {
@@ -25,6 +25,9 @@ export function LoadReflection ({  objects = [] }) {
             })
 
             if(object3D?.material){
+                if (!object3D.userData.oMaterial) {
+                    object3D.userData.oMaterial =  object3D.material
+                }
 
 
                 const reflection = reflector( { resolutionScale: .5, bounces: false, generateMipmaps: true } ); // 0.5 is half of the rendering view
@@ -45,14 +48,16 @@ export function LoadReflection ({  objects = [] }) {
 
 				const roughness = texture( perlinMap, animatedUV ).r.mul( 1 ).saturate();
 
-				const floorMaterial = new MeshPhysicalNodeMaterial().copy(object3D.material as MeshPhysicalNodeMaterial)
+				const floorMaterial = new MeshPhysicalNodeMaterial().copy(object3D.userData.oMaterial as MeshPhysicalNodeMaterial)
 				floorMaterial.transparent = true;
-				floorMaterial.colorNode = Fn( () => {
+				floorMaterial.metalnessNode = float(1.0);
+                floorMaterial.roughnessNode = roughness.mul( .2 );
 
-					// blur reflection using textureBicubic()
+                floorMaterial.colorNode = Fn( () => {
+
 					const dirtyReflection = textureBicubic( reflection, roughness.mul( .9 ) );
 
-					// falloff opacity by distance like an opacity-fog
+
 					const opacity = rangeFogFactor( 7, 25 ).oneMinus();
 
 					return vec4( dirtyReflection.rgb, opacity );
