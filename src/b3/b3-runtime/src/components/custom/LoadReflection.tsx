c7bbe966cc7eb6a7dee3bae5985bcb1959dca46d
Lok Lok <yellowhappy831@gmail.com>
2026-08-29 06:49:08 +0800

    save

--- src/b3/b3-runtime/src/components/custom/LoadReflection.tsx
+++ src/b3/b3-runtime/src/components/custom/LoadReflection.tsx
@@ -1,7 +1,7 @@
 import { useThree } from "@react-three/fiber";
 import { useEffect } from "react";
 import { Mesh,  RepeatWrapping, SRGBColorSpace, TextureLoader } from "three";
-import { Fn, vec2, vec4, texture, uv, textureBicubic, rangeFogFactor, reflector, time, float } from 'three/tsl';
+import { Fn, vec2, vec4, texture, uv, textureBicubic, rangeFogFactor, reflector, time, float, vec3 } from 'three/tsl';
 import { MeshPhysicalNodeMaterial, MeshStandardNodeMaterial } from "three/webgpu";
 
 export function LoadReflection ({  objects = [] }) {
@@ -14,25 +14,40 @@ export function LoadReflection ({  objects = [] }) {
         }
         let run = async () =>{
             //
-            let object3D = await new Promise<Mesh>((resolve, reject) => {
+            let collider = await new Promise<Mesh>((resolve, reject) => {
                 let interval = setInterval(() => {
                     let obj = scene.getObjectByName("collider")
                     if(obj){
                         clearInterval(interval)
                         resolve(obj as Mesh)
                     }
-                }, 100)
-            })
+                }, 1)
+            });
 
-            if(object3D?.material){
-                if (!object3D.userData.oMaterial) {
-                    object3D.userData.oMaterial =  object3D.material
+
+            let edge = await new Promise<Mesh>((resolve, reject) => {
+                let interval = setInterval(() => {
+                    let obj = scene.getObjectByName("edge")
+                    if(obj){
+                        clearInterval(interval)
+                        resolve(obj as Mesh)
+                    }
+                }, 1)
+            });
+
+            if(collider?.material){
+                if (!collider.userData.oMaterial) {
+                    collider.userData.oMaterial =  collider.material
                 }
 
 
                 const reflection = reflector( { resolutionScale: .5, bounces: false, generateMipmaps: true } ); // 0.5 is half of the rendering view
 				reflection.target.rotateX( - Math.PI / 2 );
 				scene.add( reflection.target );
+
+                onClean(() =>{
+                    reflection.dispose()
+                })
 
                 onClean(() =>{
                     reflection.target.removeFromParent()
@@ -48,23 +63,42 @@ export function LoadReflection ({  objects = [] }) {
 
 				const roughness = texture( perlinMap, animatedUV ).r.mul( 1 ).saturate();
 
-				const floorMaterial = new MeshPhysicalNodeMaterial().copy(object3D.userData.oMaterial as MeshPhysicalNodeMaterial)
+				const floorMaterial = new MeshPhysicalNodeMaterial().copy(collider.userData.oMaterial as MeshPhysicalNodeMaterial)
 				floorMaterial.transparent = true;
-				floorMaterial.metalnessNode = float(1.0);
-                floorMaterial.roughnessNode = roughness.mul( .2 );
+				floorMaterial.metalnessNode = float(0.2)
+                floorMaterial.roughnessNode = roughness.r.mul( .2 );
+
+                onClean(() =>{
+                    floorMaterial.dispose()
+                })
 
                 floorMaterial.colorNode = Fn( () => {
 
-					const dirtyReflection = textureBicubic( reflection, roughness.mul( .9 ) );
+					const dirtyReflection = textureBicubic( reflection, roughness.rrr.mul( .9 ) );
 
-
-					const opacity = rangeFogFactor( 5, 35 ).oneMinus();
+					const opacity = rangeFogFactor( 1, 35 ).oneMinus();
 
 					return vec4( dirtyReflection.rgb, opacity );
 
 				} )();
 
-                object3D.material = floorMaterial
+                collider.material = floorMaterial
+
+
+                const edgeMat = new MeshPhysicalNodeMaterial()
+                    
+                edgeMat.colorNode = Fn( () => {
+
+                    return vec3(1.0,1.0,0.0)
+
+				} )();
+                edgeMat.emissiveNode = Fn( () => {
+
+                    return vec3(1.0,1.0,0.0).mul(0.1)
+
+				} )();
+
+                edge.material = edgeMat
 
             }
 
