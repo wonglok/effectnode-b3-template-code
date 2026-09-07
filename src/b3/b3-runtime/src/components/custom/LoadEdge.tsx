f8b03ac00a3abedd0087b8a43c74628b0895c0f6
Lok Lok <yellowhappy831@gmail.com>
2026-09-07 22:22:45 +0800

    save

--- src/b3/b3-runtime/src/components/custom/LoadEdge.tsx
+++ src/b3/b3-runtime/src/components/custom/LoadEdge.tsx
@@ -1,14 +1,14 @@
-import { useThree } from "@react-three/fiber";
-import { useEffect, useMemo } from "react";
-import { Mesh } from "three";
-import { Fn, vec3 } from 'three/tsl';
-import { MeshPhysicalNodeMaterial } from "three/webgpu";
+import { useThree } from '@react-three/fiber'
+import { useEffect, useMemo } from 'react'
+import { Mesh } from 'three'
+import { Fn, vec3 } from 'three/tsl'
+import { MeshPhysicalNodeMaterial } from 'three/webgpu'
 // import { gaussianBlur } from 'three/addons/tsl/display/GaussianBlurNode.js';
 
-export function LoadEdge ({ texData = new Map(), objects = [] }) {
-    const scene = useThree((r) => r.scene);
+export function LoadEdge({ texData = new Map(), objects = [] }) {
+    const scene = useThree((r) => r.scene)
 
-    const done = useMemo(() =>{
+    const done = useMemo(() => {
         return new Map()
     }, [])
 
@@ -17,56 +17,53 @@ export function LoadEdge ({ texData = new Map(), objects = [] }) {
         let onClean = (v: () => void) => {
             cleans.push(v)
         }
-        let run = async () =>{
+        let run = async () => {
             const name = 'edge'
 
-            let colliderInfo = objects.find((r: any)=>{
+            let colliderInfo = objects.find((r: any) => {
                 return r.name === name
-            }) as any;
+            }) as any
 
             if (done.get(name) === colliderInfo?.version) {
                 return
-            }             
+            }
 
             let edge = await new Promise<Mesh>((resolve) => {
                 let interval = setInterval(() => {
                     let obj = scene.getObjectByName(name)
-                    if(obj){
+                    if (obj) {
                         clearInterval(interval)
                         resolve(obj as Mesh)
                     }
                 }, 1)
-            });
+            })
 
-            if(edge){
+            if (edge) {
                 const edgeMat = new MeshPhysicalNodeMaterial()
-                edgeMat.emissiveNode = Fn( () => {
-                    return vec3(1.0,1.0,0.0).mul(0.15)
-                } )();
+                edgeMat.emissiveNode = Fn(() => {
+                    return vec3(1.0, 1.0, 0.0).mul(0.15)
+                })()
 
-                onClean(() =>{
+                onClean(() => {
                     edgeMat.dispose()
                 })
-                
+
                 edge.material = edgeMat
                 done.set(name, colliderInfo?.version)
             }
         }
 
-
         run()
-        return () =>{
-            cleans.forEach((cl) =>{
+        return () => {
+            cleans.forEach((cl) => {
                 cl()
             })
         }
-    }, [objects, texData]);
+    }, [objects, texData])
 
-    return <>
-    </>;
+    return <></>
 }
 
 //
 
-
-// 
+//
