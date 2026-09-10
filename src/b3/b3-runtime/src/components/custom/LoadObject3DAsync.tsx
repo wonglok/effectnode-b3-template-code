3fcf699a8dd60869d4217d957d517b21849d4f25
Lok Lok <yellowhappy831@gmail.com>
2026-09-10 18:40:59 +0800

    save

--- src/b3/b3-runtime/src/components/custom/LoadObject3DAsync.tsx
+++ src/b3/b3-runtime/src/components/custom/LoadObject3DAsync.tsx
@@ -16,6 +16,9 @@ export function LoadObject3DAsync({ texData = new Map(), objects = [] }) {
             <group position={[0, -0.05, 0]}>
                 <SceneWaterObject name={'water2'} objects={objects}></SceneWaterObject>
             </group>
+
+            {/*  */}
+
             {/* <group position={[0, -0.05, 0]}>
                 <SceneWaterObject name={'water3'} objects={objects}></SceneWaterObject>
             </group> */}
