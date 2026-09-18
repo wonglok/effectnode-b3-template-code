4219a6fc5275ed9c581c2ba6414d01a7bd945198
Lok Lok <yellowhappy831@gmail.com>
2026-09-18 21:03:47 +0800

    100 npc

--- src/b3/b3-runtime/src/components/stores/navRigStore.ts
+++ src/b3/b3-runtime/src/components/stores/navRigStore.ts
@@ -309,7 +309,7 @@ export const useNavRigStore = create<NavRigState>((set, get) => ({
         runningSpeed: 8,
         offsetAbove: 15,
         offsetBehind: 10,
-        npcCount: 250,
+        npcCount: 100,
         npcAggroRadius: 12,
         npcScatterSeconds: 6,
         npcStandoffDistance: 5,
